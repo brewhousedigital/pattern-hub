5bfa5fbf287bdc32db84213c0f2974b6db71c23f
Brewhouse Digital <66521220+brewhousedigital@users.noreply.github.com>
2026-04-28 09:38:40 -0500

    Fixed small bug with date missing

--- src/functions/utilities/dates.ts
+++ src/functions/utilities/dates.ts
@@ -1,5 +1,9 @@
 import dayjs, { Dayjs } from 'dayjs';
 
 export const createPrettyDate = (date: string | Date | Dayjs) => {
+  if (!date) {
+    return 'No Date Selected';
+  }
+
   return dayjs(date).format('MMM DD, YYYY');
 };
